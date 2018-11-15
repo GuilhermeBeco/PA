81c6115e7930fe2131a549aac90e6baeeab19f93
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-11-15 13:58:57 +0000

    final

--- ficha6/labs/lab6/client.c
+++ ficha6/labs/lab6/client.c
@@ -1,3 +1,4 @@
+#define _DEFAULT_SOURCE		// need for random
 /**
  * @file: client.c
  * @date: 2016-11-17
@@ -9,6 +10,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
 
 #include "memory.h"
 #include "debug.h"
@@ -24,9 +28,62 @@ int main(int argc, char *argv[]){
 	    exit(ERR_ARGS);
     }
 
-    /*
-     * Inserir codigo do cliente
-     */
+    int serv_port = args_info.port_arg;
+    if( serv_port <= 0 || serv_port > MAX_PORT ){
+	    fprintf(stderr,"Invalid port given: %d (wanted:[1,%d])\n",
+			    serv_port,MAX_PORT);
+	    exit(C_ERR_INVALID_PORT);
+    }
+
+
+    int udp_server_socket;
+    if ((udp_server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+	    ERROR(C_ERR_CANT_CREATE_SOCKET,
+			    "Can't create udp_server_socket (IPv4)");
+    }
+
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET; //same as PF_INET
+    serv_addr.sin_port = htons(serv_port); 
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // network byte address
+#define C_ERR_WRONG_SERV_ADDR	(4)
+    char *serv_IP = args_info.ip_arg;
+    int ret_inet_pton = inet_pton(AF_INET,serv_IP,&(serv_addr.sin_addr.s_addr));
+    if( ret_inet_pton != 1 ){
+	    fprintf(stderr,"Wrong server address '%s'\n", serv_IP);
+	    exit(C_ERR_WRONG_SERV_ADDR);
+    }
+
+    // uint16 random number: 1...100
+    srandom(time(NULL)*getpid());
+    uint16_t my_value = (uint16_t) ((random() % 100) + 1);
+    printf("CLIENT: my_value=%d\n", (int)my_value);
+    my_value = htons(my_value);
+
+    socklen_t serv_addr_len = sizeof(serv_addr);
+    ssize_t ret_sendto = sendto(udp_server_socket,&my_value,sizeof(my_value),0,
+		    (struct sockaddr*)&serv_addr, serv_addr_len);
+    if( ret_sendto == -1 ){
+	    fprintf(stderr,"ERROR:sendto (buff=%zu bytes): %s\n",
+			    sizeof(my_value),strerror(errno));
+	    return 1;
+    }
+
+    serv_addr_len = sizeof(serv_addr);
+    char S[128];
+    ssize_t ret_recvfrom = recvfrom(udp_server_socket, S,sizeof(S),
+		    0, (struct sockaddr*)&serv_addr,&serv_addr_len);
+    if( ret_recvfrom == -1 ){
+	    fprintf(stderr,"ERROR:recvfrom (buff=%zu bytes): %s\n",
+			    sizeof(serv_addr),strerror(errno));
+	    return 1;
+    }
+
+    S[ret_recvfrom] = '\0';
+
+    printf("CLIENT: received from server '%s'\n", S);
 
     cmdline_parser_free(&args_info);
 
--- ficha6/labs/lab6/server.c
+++ ficha6/labs/lab6/server.c
@@ -10,18 +10,19 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
-#include <errno.h>
+
 #include "debug.h"
 #include "common.h"
 #include "server_opt.h"
+
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
 #include <math.h>
 
-#define C_ERR_CANT_CREATE_SOCKET (2)
-#define MAX_PORT  ((1<<16)-1)
-#define C_ERR_INVALID_PORT (1)
-#define C_ERR_CANT_CLOSE_SOCKET (3)
-#define C_ERR_CANT_BIND (4)
+#define C_CANT_BIND			(4)
 
+int handle_client(int serv_sock);
 
 int main(int argc, char *argv[]){
     /* Estrutura gerada pelo utilitario gengetopt */
@@ -31,63 +32,84 @@ int main(int argc, char *argv[]){
     if (cmdline_parser (argc, argv, &args_info) != 0){
         exit(ERR_ARGS);
     }
-    int my_port=args_info.port_arg;
-    printf("%d\n",my_port);
-    if(my_port<=0||my_port> MAX_PORT){
-      fprintf(stderr, "Invalid port given: %d (wanted:[1,%d])\n",my_port,MAX_PORT);
-      exit(C_ERR_INVALID_PORT);
+
+    int my_port = args_info.port_arg;
+    if( my_port <= 0 || my_port > MAX_PORT ){
+	    fprintf(stderr,"Invalid port given: %d (wanted:[1,%d])\n",
+			    my_port,MAX_PORT);
+	    exit(C_ERR_INVALID_PORT);
     }
 
-    //1: create socket
-    // UDP IPv4: cria socket
-
     int udp_server_socket;
-   if ((udp_server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-     ERROR(C_ERR_CANT_CREATE_SOCKET, "Can't create udp_server_socket (IPv4)");
-    //2:bind socket
+    if ((udp_server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+	    ERROR(C_ERR_CANT_CREATE_SOCKET,
+			    "Can't create udp_server_socket (IPv4)");
+    }
+    // bind
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET; //same as PF_INET
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // network byte address
+    serv_addr.sin_port = htons(my_port); 
+    printf("UDP server / port: %d\n", my_port);
+    int ret_bind = bind( udp_server_socket,
+		              (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if( ret_bind == -1 ){
+	    fprintf(stderr,"ERROR: cannot bind at port %d: %s\n",
+			    			my_port, strerror(errno));
+	    exit(C_CANT_BIND);
+    }
 
-    struct sockaddr_in udp_server_endpoint;
-   memset(&udp_server_endpoint, 0, sizeof(struct sockaddr_in));
-   udp_server_endpoint.sin_family = AF_INET;
-   udp_server_endpoint.sin_addr.s_addr = htonl(INADDR_ANY);  	// Todas as interfaces de rede
-   udp_server_endpoint.sin_port = htons(args_info.port_arg);	// Server port
-   int ret_bind=bind(udp_server_socket,(struct sockaddr *)&udp_server_endpoint,sizeof(udp_server_endpoint));
-   if(ret_bind==-1){
-     fprintf(stderr, "ERROR: cannot bind at port %d:%s\n",my_port,strerror(errno));
-     exit(C_ERR_CANT_BIND);
-   }
-   if (bind(udp_server_socket, (struct sockaddr *) &udp_server_endpoint, sizeof(struct sockaddr_in)) == -1)
-     ERROR(32, "Can't bind @udp_server_endpoint info");
 
-    //3:loop:recvfrom/sendto
+    while(1){
+	    int ret = handle_client(udp_server_socket);
+	    if( ret == -1 ){
+		    fprintf(stderr,"Problem with a client: continue\n");
+	    }
+    }
 
-    //4:close socket
-
-    // liberta recurso: socket UDP IPv4
-    if (close(udp_server_socket) == -1)
-      ERROR(C_ERR_CANT_CLOSE_SOCKET, "Can't close udp_server_socket (IPv4)");
+    if (close(udp_server_socket) == -1){
+	    ERROR(C_ERR_CANT_CLOSE_SOCKET,
+			    "Can't close udp_server_socket (IPv4)");
+    }
     cmdline_parser_free(&args_info);
 
     return 0;
 }
 
 int handle_client(int serv_sock){
-  uint16_t clnt_value;
-  struct sockaddr_in clnt_addr;
-  socklen_t clnt_addr_len=sizeof(clnt_addr);
-  char *res [];
+	uint16_t clnt_value;
+	struct sockaddr_in  clnt_addr;
+	socklen_t clnt_addr_len;
 
-  ssize_t ret_recvfrom=recvfrom(serv_sock,&clnt_value,sizeof(clnt_value),0,(struct sockaddr*)&clnt_addr,&clnt_addr_len);
-  if(ret_recvfrom==-1){
-    fprintf(stderr, "Cannot recive from server (buff=%zu):%s\n",sizeof(clnt_addr),strerror(errno));
-    return -1;
-  }
-  uint16_t host_clnt_value=ntohs(clnt_value);
-  double sqrt_clnt=sqrt(host_clnt_value);
-  snprintf(res,sizeof(sqrt_clnt),"%.5lf",sqrt_clnt);
-  ssize_t ret_sendto=sendto(serv_sock,&res,strlen(res)+1,0,(struct sockaddr*)&clnt_addr,clnt_addr_len);
-  if(ret_sendto==-1){
-    fprintf(stderr, "Cannot send to buff(=%zu)%s\n",strlen(res),strerror(errno));
-    return -1;
-  }
+	clnt_addr_len = sizeof(clnt_addr);
+	printf("SERVER: waiting for client\n");
+	ssize_t ret_recvfrom = recvfrom(serv_sock, &clnt_value,
+	   sizeof(clnt_value), 0, (struct sockaddr*)&clnt_addr,&clnt_addr_len);
+	if( ret_recvfrom == -1 ){
+		fprintf(stderr,"ERROR:recvfrom (buff=%zu bytes): %s\n",
+				sizeof(clnt_addr),strerror(errno));
+		return -1;
+	}
+
+	uint16_t host_clnt_value = ntohs(clnt_value);
+	double sqrt_clnt = sqrt(host_clnt_value);
+	char S[128];
+	snprintf(S,sizeof(S),"%lf",sqrt_clnt);
+	printf("DEBUG:sqrt_clnt='%s'\n", S);
+
+	ssize_t ret_sendto = sendto(serv_sock,S,strlen(S), 0, 
+			(struct sockaddr*)&clnt_addr, clnt_addr_len);
+
+	if( ret_sendto == -1 ){
+		fprintf(stderr,"ERROR:sendto (buff=%zu bytes): %s\n",
+				strlen(S),strerror(errno));
+		return -1;
+	}
+
+	return 0;
 }
+
+
+
+
