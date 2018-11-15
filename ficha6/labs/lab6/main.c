cc417a515206b321b36e9ae00544dd22d00dc873
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-11-15 11:57:18 +0000

    12

--- ficha6/labs/lab6/main.c
+++ ficha6/labs/lab6/main.c
@@ -1,11 +1,24 @@
-#include "debug.h"
-#include "common.h"
-#include "server_opt.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <stdin.h>
+
+#include "debug.h"
+#include "memory.h"
+#include "server_opt.h"
+
 
 #define C_ERR_CANT_CREATE_SOCKET (2)
 #define MAX_PORT  ((1<<16)-1)
 #define C_ERR_INVALID_PORT (1)
+#define C_ERR_CANT_CLOSE_SOCKET (3)
 
 int main(int argc,char* argv[]){
   struct gengetopt_agrs_info args_info;
@@ -14,6 +27,7 @@ int main(int argc,char* argv[]){
   }
 
    int my_port=args_info.port_arg;
+   printf("%d\n",my_port);
    if(my_port<=0||my_port> MAX_PORT){
      fprintf(stderr, "Invalid port given: %d (wanted:[1,%d])\n",my_port,MAX_PORT);
      exit(C_ERR_INVALID_PORT);
@@ -26,8 +40,19 @@ int main(int argc,char* argv[]){
  	if ((udp_server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
  		ERROR(C_ERR_CANT_CREATE_SOCKET, "Can't create udp_server_socket (IPv4)");
    //2:bind socket
+
+   struct sockaddr_in udp_server_endpoint;
+ 	memset(&udp_server_endpoint, 0, sizeof(struct sockaddr_in));
+ 	udp_server_endpoint.sin_family = AF_INET;
+ 	udp_server_endpoint.sin_addr.s_addr = htonl(INADDR_ANY);  	// Todas as interfaces de rede
+ 	udp_server_endpoint.sin_port = htons(args.port_arg/???);	// Server port
+ 	if (bind(udp_server_socket, (struct sockaddr *) &udp_server_endpoint, sizeof(struct sockaddr_in)) == -1)
+ 		ERROR(32, "Can't bind @udp_server_endpoint info: %s\n", strerror(errno));
    //3:loop:recvfrom/sendto
    //4:close socket
 
-
+   // liberta recurso: socket UDP IPv4
+   if (close(udp_server_socket) == -1)
+     ERROR(C_ERR_CANT_CLOSE_SOCKET, "Can't close udp_server_socket (IPv4)");
+     cmdline_parser_free(&args_info);
 }
