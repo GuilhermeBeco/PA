c2b5973739475039cbd52c1aa95748a88fdd6613
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-11-22 12:33:20 +0000

    break

--- Ficha7_Labs/lab1/server.c
+++ Ficha7_Labs/lab1/server.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include "../debug/debug.h"
 #include "../cmdline/server_cmdline.h"
 
+#define MAX_PORT ((1<<16)-1)
 
-int main(int argc, char *argv[]) 
+
+int main(int argc, char *argv[])
 {
 	struct gengetopt_args_info args;
 
 	// cmdline_parser: deve ser a primeira linha de código no main
 	if( cmdline_parser(argc, argv, &args) )
-		ERROR(99, "Erro: execução de cmdline_parser\n");
-	
+	fprintf(stderr, "cannot cmdline_parser\n");
+
+	int port = args.porto_arg;
+	if(port<=0 || port>MAX_PORT)
+	fprintf(stderr, "post %d out of range [1-%d]\n",port,MAX_PORT );
+
+	int tcp_server_socket;
+	if ((tcp_server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	ERROR(51, "Can't create tcp_server_socket (IPv4): %s\n", strerror(errno));
 
 
+	struct sockaddr_in tcp_server_endpoint;
+	memset(&tcp_server_endpoint, 0, sizeof(struct sockaddr_in));
+	tcp_server_endpoint.sin_family = AF_INET;
+	tcp_server_endpoint.sin_addr.s_addr = htonl(INADDR_ANY); 		// Todas as interfaces de rede
+	tcp_server_endpoint.sin_port = htons(port);		// Server port
+	if (bind(tcp_server_socket, (struct sockaddr *) &tcp_server_endpoint, sizeof(struct sockaddr_in)) == -1)
+	ERROR(52, "Can't bind @tcp_server_endpoint: %s\n", strerror(errno));
 
-	
+	int backlog=2;
+	if (listen(tcp_server_socket, backlog)  == -1)
+	ERROR(53, "Can't listen for %d clients: %s\n", backlog, strerror(errno));
+
+
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_len;
+	int clnt_sock;
+	const char * ret_ptr;
+	char clnt_ip_s [128];
+	while (1) {
+
+		clnt_addr_len=sizeof(clnt_addr);
+		clnt_sock=accept(tcp_server_socket,(struct sockaddr*)&clnt_addr.sin_addr.s_addr,&clnt_addr_len);
+		if(clnt_sock==-1){
+			ERROR(54,"Cannot accept");
+		}
+
+
+	ret_ptr=inet_ntop(AF_INET,&clnt_addr,clnt_ip_s,clnt_addr_len);
+	if(ret_ptr==NULL){
+		fprintf(stderr, "Cannot inet_ntop%s\n",strerror(errno));
+	}
+
+	printf("Client = %s port=%u\n",clnt_ip_s,ntohs(clnt_addr.sin_port) );
+}
+
+	close(tcp_server_socket);
 	// libertar recurso (cmdline_parser)
-	cmdline_parser_free(&args);	
+	cmdline_parser_free(&args);
 
 	exit(0);
-}	
+}
--- Ficha7_Labs/lab2/client.c
+++ Ficha7_Labs/lab2/client.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #include "../debug/debug.h"
 #include "../cmdline/client_cmdline.h"
 
-
-int main(int argc, char *argv[]) 
+#define MAX_PORT ((1<<16)-1)
+int main(int argc, char *argv[])
 {
 	struct gengetopt_args_info args;
 
 	// cmdline_parser: deve ser a primeira linha de código no main
-	if( cmdline_parser(argc, argv, &args) )
-		ERROR(99, "Erro: execução de cmdline_parser\n");
-		
-	
+	if( cmdline_parser(argc, argv, &args) ){
+		fprintf(stderr, "Erro na exec do cmdline_parser\n");
+		exit(1)	;
+	}
 
-	
+	int tcp_client_socket;
+	if ((tcp_client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	ERROR(41, "Can't create tcp_client_socket (IPv4): %s\n", strerror(errno));
+
+	int port = args.porto_arg;
+	if(port<=0 || port>MAX_PORT)
+	fprintf(stderr, "post %d out of range [1-%d]\n",port,MAX_PORT );
+	char * ip_ptr=args.ip_arg;
+
+
+	// TCP IPv4: connect ao IP/porto do servidor
+	struct sockaddr_in tcp_server_endpoint;
+	memset(&tcp_server_endpoint, 0, sizeof(struct sockaddr_in));
+	tcp_server_endpoint.sin_family = AF_INET;
+	tcp_server_endpoint.sin_port = htons(port);
+	int ret_inet=inet_pton(AF_INET, ip_ptr, &tcp_server_endpoint.sin_addr.s_addr);
+	if (ret_inet < 0){	// Server IP
+		ERROR(42, "Can't convert IP address: %s\n", strerror(errno));
+	}
+	else if (ret_inet==0){
+		fprintf(stderr, "Cannot convert ip %s\n",ip_ptr);
+		exit(32);
+	}
+	int ret_connect = connect(tcp_client_socket,(struct sockaddr*)&tcp_server_endpoint,sizeof(tcp_server_endpoint));
+	if(ret_connect==-1){
+		ERROR(54,"Cannot connect to server");
+	}
+
+
+
+
+
+
+
+	// Server port
+	/*
+	printf("a ligar ao servidor... "); fflush(stdout);
+	if (connect(tcp_client_socket, (struct sockaddr *) &tcp_server_endpoint, sizeof(struct sockaddr_in)) == -1)
+	ERROR(43, "Can't connect @tcp_server_endpoint: %s\n", strerror(errno));
+	printf("ok. \n");
+
+	*/
+
 	// libertar recurso (cmdline_parser)
-	cmdline_parser_free(&args);	
-		
+	cmdline_parser_free(&args);
+
 	exit(0);
-}	
+}
