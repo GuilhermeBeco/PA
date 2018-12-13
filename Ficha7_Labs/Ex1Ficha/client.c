6e48db3bf93e9fab7da80113d13444e0c9a6f4aa
ITzUndeeAd <guilhermebeco1999@gmail.com>
2018-12-13 13:59:14 +0000

    bota

--- Ficha7_Labs/Ex1Ficha/client.c
+++ Ficha7_Labs/Ex1Ficha/client.c
@@ -1,15 +1,16 @@
 /**
- * @file: client.c
- * @date: 2016-11-17
- * @author: autor
- */
+* @file: client.c
+* @date: 2016-11-17
+* @author: autor
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <errno.h>
+#include <sys/time.h>
 #include "memory.h"
 #include "debug.h"
 #include "common.h"
@@ -21,13 +22,67 @@ int main(int argc, char *argv[]){
 
     /* Processa os parametros da linha de comando */
     if (cmdline_parser (argc, argv, &args_info) != 0){
-	    exit(ERR_ARGS);
+        exit(ERR_ARGS);
     }
 
-    /*
-     * Inserir codigo do cliente
-     */
+    int port=args_info.port_arg;
 
+
+    int udp_client_socket;
+    if ((udp_client_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+        ERROR(EXIT_FAILURE, "Can't create udp_client_socket (IPv4): %s\n", strerror(errno));
+    }
+    struct timeval timeout;
+    timeout.tv_sec;
+    timeout.tv_usec;
+    int ret =setsockopt(udp_client_socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    if(ret==-1){
+        ERROR(EXIT_FAILURE,"Cannot set socket");
+    }
+
+    // UDP IPv4: informação do servidor UDP
+    socklen_t udp_server_endpoint_length = sizeof(struct sockaddr_in);
+    struct sockaddr_in udp_server_endpoint;
+    memset(&udp_server_endpoint, 0, sizeof(struct sockaddr_in));
+    udp_server_endpoint.sin_family = AF_INET;
+    int ret_inet=inet_pton(AF_INET,args_info.ip_arg, &udp_server_endpoint.sin_addr.s_addr);
+    if ( ret_inet==-1){
+        ERROR(EXIT_FAILURE, "Wrong family: %s\n", strerror(errno));
+
+    }
+    if(ret_inet==0){
+        ERROR(EXIT_FAILURE, "Can't convert IP address (IPv4): %s\n", strerror(errno));
+    }
+
+    udp_server_endpoint.sin_port = htons(port);
+
+    int16_t request;
+    request=0;
+
+    ssize_t udp_read_bytes, udp_sent_bytes;
+	//char buffer[];
+	// UDP IPv4: "sendto" para o servidor
+	printf("a enviar dados para o servidor... ");
+    fflush(stdout);
+    udp_sent_bytes = sendto(udp_client_socket, &request, sizeof(request), 0, (struct sockaddr *) &udp_server_endpoint, udp_server_endpoint_length);
+	if (udp_sent_bytes == -1){
+		ERROR(EXIT_FAILURE, "Can't sendto server: %s\n", strerror(errno));
+    }
+	printf("ok.  (%d bytes enviados)\n", (int)udp_sent_bytes);
+
+    printf("à espera de dados do servidor... ");
+    fflush(stdout);
+    int16_t response;
+    udp_read_bytes = recvfrom(udp_client_socket, &response, sizeof(response), 0, (struct sockaddr *) &udp_server_endpoint, &udp_server_endpoint_length);
+	if (udp_read_bytes == -1){
+		ERROR(EXIT_FAILURE, "Can't recvfrom server: %s\n", strerror(errno));
+    }
+	printf("ok.  (%d bytes recebidos)\n", (int)udp_read_bytes);
+
+
+    if (close(udp_client_socket) == -1){
+        ERROR(23, "Can't close udp_client_socket (IPv4): %s\n", strerror(errno));
+    }
     cmdline_parser_free(&args_info);
 
     return 0;
--- Ficha7_Labs/Ex1Ficha/server.c
+++ Ficha7_Labs/Ex1Ficha/server.c
@@ -1,8 +1,8 @@
 /**
- * @file: server.c
- * @date: 2016-11-17
- * @author: autor
- */
+* @file: server.c
+* @date: 2016-11-17
+* @author: autor
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -10,26 +10,92 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "debug.h"
 #include "common.h"
 #include "server_opt.h"
+#define C_MAX_STATUS ((1<<16)-1)
 
+
+void showLegenda();
+int showStatus(int casa[8]);
+int16_t check_status(int status){
+  if (status<=0||status>C_MAX_STATUS) {
+    fprintf(stderr, "ERROR: invalid status\n");
+    exit(EXIT_FAILURE);
+  }
+  return status;
+}
+int check_port(int port){
+  if (port<=0||port>C_MAX_STATUS) {
+    fprintf(stderr, "ERROR: invalid port\n");
+    exit(EXIT_FAILURE);
+  }
+  return port;
+}
 
 int main(int argc, char *argv[]){
-    /* Estrutura gerada pelo utilitario gengetopt */
-    struct gengetopt_args_info args_info;
+  /* Estrutura gerada pelo utilitario gengetopt */
+  struct gengetopt_args_info args_info;
+  int casa[8];
 
-    /* Processa os parametros da linha de comando */
-    if (cmdline_parser (argc, argv, &args_info) != 0){
-        exit(ERR_ARGS);
-    }
+  /* Processa os parametros da linha de comando */
+  if (cmdline_parser (argc, argv, &args_info) != 0){
+    exit(ERR_ARGS);
+  }
+  int16_t status = check_status(args_info.status_arg);
+  int port = check_port(args_info.port_arg);
 
-    /*
-     * Inserir codigo do servidor
-     */
+  for(int i =0;i<8;i++){
+    casa[i]= (status >> i) & 1;
+  }
+  showStatus(casa);
 
-    cmdline_parser_free(&args_info);
+  int udp_server_socket;
+  if ((udp_server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+    ERROR(31, "Can't create udp_server_socket (IPv4): %s\n", strerror(errno));
+  }
+  // UDP IPv4: bind a IPv4/porto
+  struct sockaddr_in udp_server_endpoint;
+  memset(&udp_server_endpoint, 0, sizeof(struct sockaddr_in));
+  udp_server_endpoint.sin_family = AF_INET;
+  udp_server_endpoint.sin_addr.s_addr = htonl(INADDR_ANY);  	// Todas as interfaces de rede
+  udp_server_endpoint.sin_port = htons(port);	// Server port
+  if (bind(udp_server_socket, (struct sockaddr *) &udp_server_endpoint, sizeof(struct sockaddr_in)) == -1){
+    ERROR(EXIT_FAILURE, "Can't bind @udp_server_endpoint info: %s\n", strerror(errno));
+  }
 
-    return 0;
+  if (close(udp_server_socket) == -1){
+    ERROR(EXIT_FAILURE, "Can't close udp_server_socket (IPv4): %s\n", strerror(errno));
+  }
+  cmdline_parser_free(&args_info);
+
+  return 0;
+}
+void showLegenda(){
+  printf("1: portao\n");
+  printf("2: hall\n");
+  printf("3: sala\n");
+  printf("4: jardim\n");
+  printf("5: persiana 1\n");
+  printf("6: persiana 2\n");
+  printf("7: persiana 3\n");
+  printf("8: piso\n");
+
+}
+
+int showStatus(int casa[8]){
+  showLegenda();
+  int c=1;
+  for(int i =0;i<8;i++){
+    if(casa[i]==1){
+    printf("%d está ligado\n",c);
+    c++;
+  }else{
+    printf("%d está desligado \n",c);
+    c++;
+  }
+
+  }
 }
