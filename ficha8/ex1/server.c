aad8ec33adb97d80b1232db036273b8f09d30474
ITzUndeeAd <guilhermebeco1999@gmail.com>
2019-01-03 13:35:09 +0000

    bye

--- ficha8/ex1/server.c
+++ ficha8/ex1/server.c
@@ -21,7 +21,14 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#define C_MAX_PORT ((1<<16)-1)
+#define C_IMPAR (1)
+#define C_PAR (0)
 #define MAX(x,y) (x)>(y)?(x):(y)
+int process_tcp(int tcp_server_socket);
+int process_udp(int udp_sock);
+int process_string(char *buffer,int num_type);
+int my_select(int tcp_server_socket,int udp_server_socket);
 int check_port(int port){
    if (port<=0||port>C_MAX_PORT) {
       fprintf(stderr, "ERROR: invalid port\n");
@@ -99,7 +106,7 @@ int my_select(int tcp_server_socket,int udp_server_socket){
       // ativa monitorização para os descritores sock_UDP, sock_TCP1 e sock_TCP2.
       FD_SET(udp_server_socket,&readset);
       FD_SET(tcp_server_socket,&readset);
-      timeout.tv_sec = 30;
+      timeout.tv_sec = 10;
       timeout.tv_usec = 0;
       int ret_select = select(max+1, &readset, NULL, NULL, &timeout);
       if(ret_select == -1 ){
@@ -111,12 +118,90 @@ int my_select(int tcp_server_socket,int udp_server_socket){
       }
       if( FD_ISSET(udp_server_socket, &readset) ){
          printf("Event in sock_UDP\n");
+         process_udp(udp_server_socket);
          continue;
       }
       if( FD_ISSET(tcp_server_socket,&readset) ){
          printf("Event in sock_TCP1\n");
+         process_tcp(tcp_server_socket);
          continue;
       }
    }// while (1);
+
+}
+
+int process_udp(int udp_sock){
+   socklen_t udp_client_endpoint_length = sizeof(struct sockaddr_in);
+   struct sockaddr_in udp_client_endpoint;
+   ssize_t udp_read_bytes, udp_sent_bytes;
+   char buffer[256];
+   //...
+
+   // UDP IPv4: "recvfrom" do cliente (bloqueante)
+   printf("à espera de dados do cliente... "); fflush(stdout);
+   if ((udp_read_bytes = recvfrom(udp_sock, buffer, sizeof(buffer)-1, 0, (struct sockaddr *) &udp_client_endpoint, &udp_client_endpoint_length)) == -1){
+      ERROR(34, "Can't recvfrom client: %s\n", strerror(errno));
+   }
+   printf("ok.  (%d bytes recebidos)\n", (int)udp_read_bytes);
+   buffer[udp_read_bytes]='\0';
+
+   int num_ret=process_string(buffer,C_IMPAR);
+   snprintf(buffer,sizeof(buffer), "udp: %d\n",num_ret);
+
+   // UDP IPv4: "sendto" para o cliente
+   printf("a enviar dados para o cliente... "); fflush(stdout);
+   if ((udp_sent_bytes = sendto(udp_sock, buffer, strlen(buffer), 0, (struct sockaddr *) &udp_client_endpoint, udp_client_endpoint_length)) == -1){
+      ERROR(35, "Can't sendto client: %s\n", strerror(errno));
+   }
+   printf("ok.  (%d bytes enviados)\n", (int)udp_sent_bytes);
+
+}
+int process_tcp(int tcp_server_socket){
+   
+   char tcp_client_string_ip[20];
+   struct sockaddr_in tcp_client_endpoint;
+   socklen_t tcp_client_endpoint_length = sizeof(struct sockaddr_in);
+   int tcp_client_socket;
+
+   printf("à espera da ligação do cliente... "); fflush(stdout);
+   if ((tcp_client_socket = accept(tcp_server_socket, (struct sockaddr *) &tcp_client_endpoint, &tcp_client_endpoint_length)) == -1)
+   ERROR(54, "Can't accept client: %s\n", strerror(errno));
+   printf("ok. \n");
+   printf("cliente: %s@%d\n", inet_ntop(AF_INET, &tcp_client_endpoint.sin_addr, tcp_client_string_ip, sizeof(tcp_client_string_ip)), htons(tcp_client_endpoint.sin_port));
+
+
+   ssize_t tcp_read_bytes, tcp_sent_bytes;
+   char buffer[256];
+   // TCP IPv4: "recv" do cliente (bloqueante)
+   printf("à espera de dados do cliente... "); fflush(stdout);
+   if ((tcp_read_bytes = recv(tcp_client_socket, buffer, sizeof(buffer)-1, 0)) == -1)
+   ERROR(57, "Can't recv from client: %s\n", strerror(errno));
+   printf("ok.  (%d bytes recebidos)\n", (int)tcp_read_bytes);
+
+   buffer[tcp_read_bytes]='\0';
+   int num_ret=process_string(buffer, C_PAR);
+   snprintf(buffer, sizeof(buffer), "TCP: %d\n",num_ret);
+
+   // TCP IPv4: "send" para o cliente
+   printf("a enviar dados para o cliente... "); fflush(stdout);
+   if ((tcp_sent_bytes = send(tcp_client_socket, buffer, strlen(buffer), 0)) == -1)
+   ERROR(58, "Can't send to client: %s\n", strerror(errno));
+   printf("ok.  (%d bytes enviados)\n", (int)tcp_sent_bytes);
+
+   if (close(tcp_client_socket) == -1){
+      ERROR(55, "Can't close tcp_client_socket (IPv4): %s\n", strerror(errno));
+   }
+}
+int process_string(char *buffer,int num_type){
+   int num=atoi(buffer);
+   if(num<=0){
+      fprintf(stderr, "Num invalid \n");
+      return 0;
+   }
+   int ret_num = rand() % num;
+   if(!((ret_num%2)==num_type)){
+      ret_num++;
+   }
+   return ret_num;
 
 }
